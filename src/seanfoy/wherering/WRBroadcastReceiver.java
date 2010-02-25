f67a5052b74b7f1b119185e1553cf2837becf562
Sean Foy <sean.foy@gmail.com>
2010-02-25 10:04:24 -0600

    Try to let proximity alerts die when the app is uninstalled. It doesn't actually work, I think due to a bug in the system LocationManagerService. I'm preparing a testcase/patch for that now.

--- src/seanfoy/wherering/WRBroadcastReceiver.java
+++ src/seanfoy/wherering/WRBroadcastReceiver.java
@@ -1,12 +1,15 @@
 package seanfoy.wherering;
 
 import static seanfoy.wherering.intent.IntentHelpers.fullname;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashSet;
 import java.util.Set;
 import seanfoy.Greenspun.Func1;
 import seanfoy.wherering.intent.action;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -16,30 +19,72 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.media.AudioManager;
+import android.os.SystemClock;
+import android.util.Log;
 
 public class WRBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(fullname(action.PROXIMITY))) {
+        String intentAction = intent.getAction();
+        if (intentAction.equals(fullname(action.PROXIMITY))) {
             updateRing(context, intent);
             context.sendBroadcast(new Intent(fullname(action.ALERT)));
         }
-        else if (intent.getAction().equals(fullname(action.ALERT))) {
+        else if (intentAction.equals(fullname(action.ALERT))) {
             raiseAlert(context);
         }
-        else if (intent.getAction().equals(fullname(action.SHUTDOWN))) {
+        else if (intentAction.equals(fullname(action.SHUTDOWN))) {
+            AlarmManager am =
+                (AlarmManager)getSystemService(context, Context.ALARM_SERVICE);
+            synchronized(alarmSubs) {
+                for (PendingIntent a : alarmSubs) {
+                    Log.i(getClass().getName(), "cancelling an alarm");
+                    am.cancel(a);
+                }
+                alarmSubs.clear();
+            }
             unsubscribe(context);
         }
-        else if (intent.getAction().equals(android.content.Intent.ACTION_BOOT_COMPLETED)) {
+        else if (intentAction.equals(fullname(action.SUBSCRIBE))) {
+            unsubscribe(context);
+            subscribe(context, getConfig(context));
+        }
+        else if (intentAction.equals(android.content.Intent.ACTION_BOOT_COMPLETED)) {
             if (!getPrefs(context).getBoolean(android.content.Intent.ACTION_BOOT_COMPLETED, true)) {
                 return;
             }
-            subscribe(context, getConfig(context));
+            context.sendBroadcast(new Intent(fullname(action.STARTUP)));
         }
-        else if (intent.getAction().equals(fullname(action.STARTUP))) {
-            subscribe(context, getConfig(context));
+        else if (intentAction.equals(fullname(action.STARTUP))) {
+            // If this app is uninstalled, its
+            // non-expiring PAs will persist
+            // until reboot. So, the PAs ought
+            // to expire and we'll renew them
+            // periodically before the
+            // expirations and until we're
+            // uninstalled or shutdown.
+            AlarmManager am =
+                (AlarmManager)getSystemService(context, Context.ALARM_SERVICE);
+            PendingIntent ai =
+                PendingIntent.getBroadcast(
+                    context.getApplicationContext(),
+                    // not used --- Docs
+                    0,
+                    new Intent(fullname(action.SUBSCRIBE)),
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            alarmSubs.add(ai);
+            am.setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime(),
+                LM_EXPIRY / 2,
+                ai);
         }
     }
+    
+    private Set<PendingIntent> alarmSubs =
+        Collections.synchronizedSet(new HashSet<PendingIntent>());
+    private static final long LM_EXPIRY =
+        AlarmManager.INTERVAL_FIFTEEN_MINUTES;
     
     private void raiseAlert(Context context) {
             String app_name = context.getString(R.string.app_name);
@@ -62,9 +107,8 @@ public class WRBroadcastReceiver extends BroadcastReceiver {
             nm.notify(1, note);
     }
 
-    private static volatile Object lmsubsLock = new Object();
     private final static Set<PendingIntent> lmsubs =
-        new HashSet<PendingIntent>(); 
+        Collections.synchronizedSet(new HashSet<PendingIntent>());
 
     @SuppressWarnings("unchecked")
     public static <T> T getSystemService(Context ctx, String name) {
@@ -73,42 +117,35 @@ public class WRBroadcastReceiver extends BroadcastReceiver {
     
     private static final String placeKeyName = WRBroadcastReceiver.class.getName() + ":place-key";
     private void subscribe(Context ctx, HashMap<Integer, Place> config) {
+        Log.i(getClass().getName(), String.format("System.currentTimeMillis() = %s", System.currentTimeMillis()));
         LocationManager lm = getSystemService(ctx, Context.LOCATION_SERVICE);
-        synchronized(lmsubsLock) {
-            for (Map.Entry<Integer, Place> c : config.entrySet()) {
-                Intent i = new Intent(fullname(action.PROXIMITY));
-                i.putExtra(placeKeyName, c.getKey());
-                PendingIntent pi =
-                    PendingIntent.getBroadcast(
-                        ctx,
-                        // docs say "currently not used"
-                        0,
-                        i,
-                        0);
-                lmsubs.add(pi);
-                lm.addProximityAlert(
-                    c.getValue().location.getLatitude(),
-                    c.getValue().location.getLongitude(),
-                    radiusM,
-                    // never time out
-                    // If this app is killed, the
-                    // PA will persist until reboot.
-                    // Maybe it would be better to
-                    // let this PA die periodically
-                    // with replacement, courtesy of
-                    // an Alarm.
-                    -1,
-                    pi);
-            }
+        for (Map.Entry<Integer, Place> c : config.entrySet()) {
+            Intent i = new Intent(fullname(action.PROXIMITY));
+            i.putExtra(placeKeyName, c.getKey());
+            PendingIntent pi =
+                PendingIntent.getBroadcast(
+                    ctx,
+                    // docs say "currently not used"
+                    0,
+                    i,
+                    0);
+            lmsubs.add(pi);
+            lm.addProximityAlert(
+                c.getValue().location.getLatitude(),
+                c.getValue().location.getLongitude(),
+                radiusM,
+                System.currentTimeMillis() + LM_EXPIRY,
+                pi);
         }
     }
 
     private void unsubscribe(Context ctx) {
         LocationManager lm = getSystemService(ctx, Context.LOCATION_SERVICE);
-        synchronized(lmsubsLock) {
+        synchronized(lmsubs) {
             for (PendingIntent i : lmsubs) {
                 lm.removeProximityAlert(i);
             }
+            lmsubs.clear();
         }
     }
     
--- src/seanfoy/wherering/intent/action.java
+++ src/seanfoy/wherering/intent/action.java
@@ -1,8 +1,32 @@
 package seanfoy.wherering.intent;
 
 public enum action {
+    /**
+     * Subscribe and periodically renew
+     * subscriptions to proximity alerts
+     */
 	STARTUP,
+	/**
+	 * Cancel proximity subscriptions
+	 * and don't renew them.
+	 */
 	SHUTDOWN,
+	/**
+	 * Set the ringer mode according to
+	 * the user's notable places
+	 * configuration
+	 */
 	PROXIMITY,
-	ALERT
+	/**
+	 * Notify the user that the ringer
+	 * mode has been changed by this
+	 * app
+	 */
+	ALERT,
+	/**
+	 * Establish non-recurring proximity
+	 * alerts according to the user's
+	 * notable places configuration
+	 */
+	SUBSCRIBE
 }
